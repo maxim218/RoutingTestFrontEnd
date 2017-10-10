faece7de4d5b404fdf636ef65faf609e35edf39f
maxim218 <31542700+maxim218@users.noreply.github.com>
2017-10-10 21:44:32 +0300

    Add universal link meeting
    Add universal link meeting

--- index.js
+++ index.js
@@ -3,25 +3,11 @@
 let express = require("express");
 let app = express();
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
     app.use(express.static(__dirname + "/static"));
     res.sendfile("static/index.html");
 });
 
-app.get('/box1', function(req, res) {
-    app.use(express.static(__dirname + "/static"));
-    res.sendfile("static/index.html");
-});
-
-app.get('/box2', function(req, res) {
-    app.use(express.static(__dirname + "/static"));
-    res.sendfile("static/index.html");
-});
-
-app.get('/box3', function(req, res) {
-    app.use(express.static(__dirname + "/static"));
-    res.sendfile("static/index.html");
-});
 
 let port = process.env.PORT || 5000;
 app.listen(port);
